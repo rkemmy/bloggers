e77f922d1d8fc4e4ced8f518ed780afb3eb0a171
rkemmy <rkemmy.rk69@gmail.com>
2018-04-22 19:52:04 +0300

    Import loginmanager and instantiate app

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,26 @@
-
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
-def create_app(config_state):
+
+def create_app(config_name):
     app = Flask(__name__)
-    app.config.from_object(config_options[config_state])
 
-
+    app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     #Registering blueprints
     from .main import main as main_blueprint
