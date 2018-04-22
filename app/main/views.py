82409dde7ce9954366e2f6a737ee53ead234d78d
rkemmy <rkemmy.rk69@gmail.com>
2018-04-22 19:56:34 +0300

    define template to be displayed for current user

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,10 @@
-from . import main
+from flask_login import current_user, login_required
 from flask import render_template
+from . import main
 
 @main.route('/')
+@login_required
 def index():
     return render_template('index.html')
+
+# @main.route()
