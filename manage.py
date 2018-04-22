1c01cdaede8ac1abcfc09a134b4499625efb263f
rkemmy <rkemmy.rk69@gmail.com>
2018-04-22 19:49:42 +0300

    Import user and migratecommand

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
 
-from flask_script import Manager,Server
-from app import create_app,db
+from flask_script import Manager, Server
+from app import create_app, db
+from app.models import User
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('default')
 
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
