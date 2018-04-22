7bc6413fdfcd3e1418618e309e5ea7d28af147ec
rkemmy <rkemmy.rk69@gmail.com>
2018-04-22 19:55:27 +0300

    define routes for login and logout

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,41 @@
-from .forms import LoginForm
+from flask import render_template, redirect, url_for
+from flask_login import login_user,logout_user,login_required
+from .forms import LoginForm, RegistrationForm
 from . import auth
-from .models import User
+from app.models import User
+from app import db
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(url_for('main.index'))
+    return render_template('auth/login.html', login_form = form)
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(
+            email=form.email.data,
+            username = form.username.data,
+            password = form.password.data
+        )
+
+        db.create_all()
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    return render_template('auth/register.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
