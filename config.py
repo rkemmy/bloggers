670ede2a84ab86c1e43a6a2859d1dba9594cf652
rkemmy <rkemmy.rk69@gmail.com>
2018-04-22 19:49:04 +0300

    include secretkey and sql configurations

--- config.py
+++ config.py
@@ -1,11 +1,25 @@
 class Config:
-    pass
+    SECRET_KEY = 'amsecret'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 class ProdConfig(Config):
-    pass
+    DEBUG = False
+
 
 class DevConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://remmy:bxt@localhost/bloggin'
 
-config_options ={"production":ProdConfig,"default":DevConfig}
 
+
+
+
+config_options = {
+  "production":ProdConfig,
+  "default":DevConfig
+}
