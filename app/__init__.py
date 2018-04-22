3e9db9a2aee80046eb9c753fed212ead9d6a556e
rkemmy <rkemmy.rk69@gmail.com>
2018-04-22 09:26:50 +0300

    Register the blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,12 @@ def create_app(config_state):
     bootstrap.init_app(app)
     db.init_app(app)
 
+    #Registering blueprints
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
 
     return app
