2d1e80f3df1f86dfbc67ada87cb003073908bdac
rkemmy <rkemmy.rk69@gmail.com>
2018-04-23 00:43:09 +0300

    create a form for posts

---
+++ app/main/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField,StringField, TextAreaField
+from wtforms.validators import Required, Length
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[Required(), Length(min=4, max=50)])
+    body = TextAreaField('Body', validators=[Required(),Length(min=1, max=400)])
+    submit = SubmitField('Create Post')
