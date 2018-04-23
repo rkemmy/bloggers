e774852243c756e3a619b32b894369e77b490375
rkemmy <rkemmy.rk69@gmail.com>
2018-04-23 14:48:56 +0300

    Create a comment class

--- app/models.py
+++ app/models.py
@@ -40,11 +40,26 @@ class Post(db.Model):
     body = db.Column(db.String(400))
     timestamp = db.Column(db.DateTime, index = True, default = datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments = db.relationship('Comment', backref = 'comment', lazy='dynamic')
 
     # parse timestamp
+    @staticmethod
     def time_parser(time_str):
-        pass
+        # TODO
+        # use hours ago if less than 24 hours $ days afterwards
+        new_date = time_str.date()
+        return new_date
 
     def __repr__(self):
         return '<Posts {}>'.format(self.title)
 
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50), unique=True)
+    description = db.Column(db.String(400))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+    def __repr__(self):
+        return  '<Comments {}>'.format(self.description)
