dd02f2b1c1e5e0629ba43aff6f315d9aa2d6d650
rkemmy <rkemmy.rk69@gmail.com>
2018-04-23 14:49:51 +0300

    Create a form for the comments

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,3 +6,9 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[Required(), Length(min=4, max=50)])
     body = TextAreaField('Body', validators=[Required(),Length(min=1, max=400)])
     submit = SubmitField('Create Post')
+
+
+class CommentForm(FlaskForm):
+    username = StringField('Name', validators=[Required()])
+    description = TextAreaField('Comment', validators=[Required(), Length(min=1, max=400)])
+    submit = SubmitField('Submit')
